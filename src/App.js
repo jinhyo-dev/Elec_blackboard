d2a44b2c224b0ab7d87edfd3cd8aa98d6b7eaae4
jinhyo-dev <jinhyo0850@gmail.com>
2022-01-27 00:18:03 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [timer, setTimer] = useState('00:00:00')
+
+  const currentTimer = () => {
+    const date = new Date()
+    const tmp_day = String(date).toUpperCase()
+    const day = tmp_day.substring(0,3)
+    const hours = String(date.getHours()).padStart(2, '0')
+    const minutes = String(date.getMinutes()).padStart(2, '0')
+    setTimer (`(${day}) ${hours}:${minutes}`)
+  }
+
+  const startTimer = () => {
+    setInterval(currentTimer, 1000)
+  }
+
+  startTimer()
+
+  return (
+    <div className="main">
+      <h1>{timer}</h1>
+    </div>
+  );
+}
+
+export default App;
