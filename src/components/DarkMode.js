60ed5bfc00d807b3b8c0a7db851b9626671291d2
jinhyo-dev <jinhyo0850@gmail.com>
2022-01-29 19:38:43 +0900

    commit

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -4,17 +4,22 @@ import '../styles/DarkMode.css';
 export default function DarkMode() {
 
   const darkmode = () => {
-    const checkbox = document.querySelector('.check');
+    const checkbox = document.querySelector('.check')
     const body = document.body
-    const time = document.querySelector('.time');
+    const time = document.querySelector('.time')
+    const setMode = document.querySelector('.setMode')
 
     checkbox.addEventListener('click', e => {
       if (e.target.checked) {
         body.style.backgroundColor = 'black'
         time.style.color = 'white'
+        setMode.innerHTML = 'Black Mode'
+        setMode.style.color = 'white'
       } else {
         body.style.backgroundColor = 'white'
         time.style.color = 'black'
+        setMode.innerHTML = 'Light Mode'
+        setMode.style.color = 'black'
       }
     });
 
@@ -22,6 +27,7 @@ export default function DarkMode() {
 
   return (
     <div>
+      <div className="setMode">Light Mode</div>
       <label class="switch-button">
         <input className='check' type="checkbox" onClick={darkmode}/>
         <span class="onoff-switch"></span>
