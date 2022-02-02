53a98b6974c2136aa27b3fad15ec9a513a7e2706
doc <l01062506145@gmail.com>
2022-02-02 23:18:40 +0900

    plz insert icon in footer 2/2 11:18

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,22 @@
 // import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styles/footer.css';
 
-export default function footer() {
-  return (
+
+const footer = () => {
+  const activeStyle = {
+    color: 'green'
+  }
+  return(
     <div className="footer">
-      <p>© 2022 - GBSW High School</p>
+      <div className='icoFlax'>
+        <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
+        <NavLink exact to="/food" activeStyle={activeStyle}>Food</NavLink>
+        <NavLink exact to="/notice" activeStyle={activeStyle}>Notice</NavLink>
+
+      </div>
+      <p className='copyRight'>© 2022 - GBSW High School</p>
     </div>
   )
-
 }
+export default footer
