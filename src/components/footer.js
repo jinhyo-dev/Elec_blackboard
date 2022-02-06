836e1c364bfcdecfb01873a010014be361ca321f
jinhyo-dev <jinhyo0850@gmail.com>
2022-02-06 18:44:22 +0900

    color change button 2/6 18:44

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,24 +12,35 @@ const footer = () => {
     color: 'green'
   }
 
-  // const clickEvent = () => {
-  //   const home = document.querySelector('.Home-Icon')
-    
-  // }
+  const HomeClickEvent = () => {
+    const home = document.querySelector('.Home-Icon')
+    home.style.color = 'yellow'
+    // home.style.color = 'blue'  
+  }
+
+  const FoodClickEvent = () => {
+    const food = document.querySelector('.Food-Icon')
+    food.style.color = 'orange'
+  }
+
+  const NoticeClickEvent = () => {
+    const notice = document.querySelector('.Notice-Icon')
+    notice.style.color = 'green'
+  }
 
   return(
     <div className="footer">
       <div className='icoFlax'>
           <NavLink exact to="/" activeStyle={activeStyle} className="navbar-home">
-            <BiHome size='40' color='#22a7f2' className='Home-Icon' />
+            <BiHome size='40' color='#22a7f2' className='Home-Icon' onClick={() => HomeClickEvent()} />
           </NavLink>
 
         <NavLink exact to="/food" activeStyle={activeStyle} className="navbar-food">
-          <MdOutlineFoodBank size='40' color='#36d128' className='Food-Icon'/>
+          <MdOutlineFoodBank size='40' color='#36d128' className='Food-Icon' onClick={() => FoodClickEvent()}/>
         </NavLink>
 
         <NavLink exact to="/notice" activeStyle={activeStyle} className="navbar-notice">            
-          <FiBell size='40' color='#eb9021' className='Notice-Icon'/>
+          <FiBell size='40' color='#eb9021' className='Notice-Icon' onClick={() => NoticeClickEvent()}/>
         </NavLink>          
             
       </div>
