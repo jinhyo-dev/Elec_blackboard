28707c668d30144682210b42ad31736604a9b09a
jinhyo-dev <jinhyo0850@gmail.com>
2022-02-13 21:59:53 +0900

    Foodjs  make table 2/13 21:59

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -3,8 +3,21 @@ import '../styles/pages.css';
 
 export default function food() {
   return (
-    <div className='middle-page'>
-      food
+    <div className='Food'>
+      <table className='FoodTable'>
+        <thead>
+          <th>날짜</th>
+          <th>아침</th>
+          <th>점심</th>
+          <th>저녁</th>
+        </thead>
+        <tr>
+          <td>2월 13일</td>
+          <td>girl</td>
+          <td>girl2</td>
+          <td>girl3</td>
+        </tr>
+      </table>
     </div>
   )
 }
