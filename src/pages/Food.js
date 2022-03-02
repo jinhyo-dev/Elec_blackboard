cb58093e1795b860fce11ff72f2c3f14461b1bad
jinhyo-dev <jinhyo0850@gmail.com>
2022-03-02 22:05:31 +0900

    3/2 22;:04 food api Error occur

--- src/pages/Food.js
+++ src/pages/Food.js
@@ -9,14 +9,37 @@ export default function Food() {
   today = year + month + day
   const [date, setDate] = useState("")
   const delStr = /[^0-9]/g
+
   const checkDate = () => {
     console.log(date.replace(delStr, ''))
     console.log(today)
   }
+
+  const showMeal = () => {
+    var xhr = new XMLHttpRequest();
+    var day = "20220303";
+    var target = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=99a1b8c7095144abae21e03b2a2f65f3&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8750767&MLSV_YMD=${today}`;
+    // xhr.open("GET", target + "/MLSV_YMD=" + day);
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status == 200) {
+          let loadedJSON = JSON.parse(xhr.responseText);
+          console.log(loadedJSON.mealServiceDietInfo[1].row[0].DDISH_NM)//아침
+          console.log(loadedJSON.mealServiceDietInfo[1].row[1].DDISH_NM)//점심
+          console.log(loadedJSON.mealServiceDietInfo[1].row[2].DDISH_NM)//저녁
+
+        } else {
+          alert("fail to load");
+        }
+      }
+    }
+  }
   return (
     <div className='Food'>
       <input type='date' onChange={(e) => setDate(e.target.value)}></input>
-      <button onClick={checkDate}>yes</button>
+      <button onClick={() => { checkDate(), showMeal() }}>yes</button>
     </div>
   )
 }
